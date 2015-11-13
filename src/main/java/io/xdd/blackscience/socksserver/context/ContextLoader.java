1b9cab58032962c27a8eaacf25d9bab0f2d7d67c
jianbo <387852424@qq.com>
2015-11-13 11:14:05 +0800

    fix

---
+++ src/main/java/io/xdd/blackscience/socksserver/context/ContextLoader.java
@@ -0,0 +1,15 @@
+package io.xdd.blackscience.socksserver.context;
+
+
+import java.util.Properties;
+
+/**
+ * web context to Manager SocksServer
+ * */
+public class ContextLoader{
+
+
+    public void initSocksServer(Properties properties){
+
+    }
+}
--- src/main/java/io/xdd/blackscience/socksserver/context/ContextLoaderListener.java
+++ src/main/java/io/xdd/blackscience/socksserver/context/ContextLoaderListener.java
@@ -1,9 +1,10 @@
-package io.xdd.blackscience.socksserver.webcontext;
+package io.xdd.blackscience.socksserver.context;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-public class ContextLoader implements ServletContextListener {
+public class ContextLoaderListener extends ContextLoader implements ServletContextListener{
+
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
