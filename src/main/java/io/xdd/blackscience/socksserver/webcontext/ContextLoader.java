f57146deee7f6763631aec53363c0f63c8e4f680
jianbo <387852424@qq.com>
2015-11-13 11:02:52 +0800

    init

---
+++ src/main/java/io/xdd/blackscience/socksserver/webcontext/ContextLoader.java
@@ -0,0 +1,17 @@
+package io.xdd.blackscience.socksserver.webcontext;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ContextLoader implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
